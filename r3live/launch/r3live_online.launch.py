36a2d0803c6ae9e2c1327e2ab3d45d6721ee3415
vlacorre <vivien.lacorre@chalmers.se>
2024-09-18 16:58:19 +0200

    update for substitute

--- r3live/launch/r3live_online.launch.py
+++ r3live/launch/r3live_online.launch.py
@@ -9,8 +9,8 @@ from launch_ros.actions import Node
 
 ### MANUAL SETTINGS
 # r3live_config_file = 'r3live_config_MPIR.yaml'
-r3live_config_file = 'r3live_config_gazebo_sim.yaml'
-# r3live_config_file = 'r3live_config_substitute.yaml' # MPIR substitute
+# r3live_config_file = 'r3live_config_gazebo_sim.yaml'
+r3live_config_file = 'r3live_config_substitute.yaml' # MPIR substitute
 
 def generate_launch_description():
   # Subscribed topics
